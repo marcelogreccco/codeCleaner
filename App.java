5dd3012951b2790d2998936f716e31148c562af1
Marcelo Grecco Magalhaes <marcelo.grecco@gmail.com>
2019-10-28 22:27:33 -0300

    versao inicial

---
+++ App.java
@@ -0,0 +1,22 @@
+expackage br.com.otimize.codecleaner;
+
+
+public class App 
+{
+	public static void main( String[] args )
+    {
+		Integer numeroUm = 3;
+		Integer numeroDois = 4;
+		
+    	System.out.println( "Hello World!" );
+
+    	if (numeroUm.compareTo(4)==0) {
+    		System.out.println( "Hello World!" );
+    	}
+    	
+    	for(int i = 0; i < 4; i++){
+    		System.out.println( "Hello World!" );
+    	}
+    }
+}
+
